24a1080e1737d83588887ad579cc912d99723ec1
mchetelat <m.chetelat@me.com>
2018-02-15 16:59:08 +0100

    Update

--- main.go
+++ main.go
@@ -21,8 +21,8 @@ import (
 )
 
 var (
-	logger *log.Logger
-	openTx = make(map[[32]byte]*protocol.FundsTx)
+	logger  *log.Logger
+	openTxs = make(map[[32]byte]*protocol.FundsTx)
 )
 
 func main() {
@@ -36,9 +36,11 @@ func main() {
 }
 
 func listener() {
-	listener, err := net.Listen("tcp", ":8002")
+	listener, err := net.Listen("tcp", client.MULTISIG_SERVER_PORT)
 	if err != nil {
 		logger.Fatal(err)
+	} else {
+		logger.Printf("Listening on port %v", client.MULTISIG_SERVER_PORT)
 	}
 
 	for {
@@ -56,81 +58,107 @@ func serve(c net.Conn) {
 	header, payload, err := client.RcvData(c)
 	if err != nil {
 		logger.Printf("Failed to handle incoming connection: %v\n", err)
+		c.Close()
 		return
 	}
 
 	if header.TypeID == p2p.FUNDSTX_BRDCST {
-		var fundsTx *protocol.FundsTx
+		var tx *protocol.FundsTx
 
-		if fundsTx = fundsTx.Decode(payload); fundsTx == nil {
+		if tx = tx.Decode(payload); tx == nil {
+			c.Close()
 			return
 		}
 
 		packet := p2p.BuildPacket(p2p.TX_BRDCST_ACK, nil)
 		c.Write(packet)
 
-		processTx(fundsTx)
+		txHash := tx.Hash()
+		if err := processTx(tx); err != nil {
+			logger.Printf("Processing tx %x failed: %v", txHash[:8], err)
+		}
 	}
 
 	c.Close()
 }
 
-func processTx(tx *protocol.FundsTx) {
+func processTx(tx *protocol.FundsTx) (err error) {
 	acc, err := reqAccount(tx.From)
 	if err != nil {
+		return err
 	}
 
-	if checkSolvency(tx.From, tx.Amount, acc) {
-		openTx[tx.Hash()] = tx
+	if checkSolvency(tx, acc) {
+		openTxs[tx.Hash()] = tx
 
-		signTx(tx)
+		if err := signTx(tx); err != nil {
+			return err
+		}
 
-		sendTx(tx)
+		if err := sendTx(tx); err != nil {
+			return err
+		}
 	}
+
+	return nil
 }
 
-func checkSolvency(pubKeyHash [32]byte, amount uint64, acc client.Account) bool {
+func checkSolvency(tx *protocol.FundsTx, acc *client.Account) bool {
+	//Use signed int, since otherwise it might happen that a negative balance will become positive
+	var tmpBalance int64
+
 	solvent := false
-	tmpBalance := acc.Balance
+	tmpBalance = int64(acc.Balance)
 
 	if !acc.IsRoot {
-		for _, fundsTx := range openTx {
-			if fundsTx.From == pubKeyHash {
-				tmpBalance -= fundsTx.Amount
+		for _, openTx := range openTxs {
+			if openTx.From == tx.From {
+				tmpBalance -= int64(tx.Amount)
 			}
-			if fundsTx.To == pubKeyHash {
-				tmpBalance += fundsTx.Amount
+			if openTx.To == tx.From {
+				tmpBalance += int64(tx.Amount)
 			}
 		}
 	}
 
-	if tmpBalance >= amount || acc.IsRoot {
+	if tmpBalance >= int64(tx.Amount)+int64(tx.Fee) || acc.IsRoot {
 		solvent = true
+	} else {
+		logger.Printf("Account %x is not solvent", tx.From[:8])
 	}
 
 	return solvent
 }
 
-func reqAccount(pubKeyHash [32]byte) (acc client.Account, err error) {
-	response, err := http.Get("http://" + client.LIGHT_CLIENT_SERVER + "/account/" + hex.EncodeToString(pubKeyHash[:]))
+func reqAccount(addressHash [32]byte) (acc *client.Account, err error) {
+	response, err := http.Get("http://" + client.LIGHT_CLIENT_SERVER + "/account/" + hex.EncodeToString(addressHash[:]))
 	if err != nil {
-		return acc, errors.New(fmt.Sprintf("The HTTP request failed with error %s\n", err))
+		return nil, errors.New(fmt.Sprintf("The HTTP request failed with error %s", err))
 	}
 
 	data, _ := ioutil.ReadAll(response.Body)
 	json.Unmarshal([]byte(data), &acc)
 
+	if !acc.IsCreated {
+		return nil, errors.New(fmt.Sprintf("Account %x not found", addressHash[:8]))
+	}
+
 	return acc, nil
 }
 
-func signTx(tx *protocol.FundsTx) {
+func signTx(tx *protocol.FundsTx) error {
 	_, privKey, _ := storage.ExtractKeyFromFile(os.Args[1])
 
 	txHash := tx.Hash()
-	r, s, _ := ecdsa.Sign(rand.Reader, &privKey, txHash[:])
+	r, s, err := ecdsa.Sign(rand.Reader, &privKey, txHash[:])
+	if err != nil {
+		return errors.New(fmt.Sprintf("Could not sign tx: %v", err))
+	}
 
 	copy(tx.Sig2[32-len(r.Bytes()):32], r.Bytes())
 	copy(tx.Sig2[64-len(s.Bytes()):], s.Bytes())
+
+	return nil
 }
 
 func sendTx(tx *protocol.FundsTx) error {
@@ -140,13 +168,15 @@ func sendTx(tx *protocol.FundsTx) error {
 	txHash := tx.Hash()
 	response, err := http.Post("http://"+client.LIGHT_CLIENT_SERVER+"/sendFundsTx/"+hex.EncodeToString(txHash[:])+"/"+hex.EncodeToString(tx.Sig2[:]), "application/json", bytes.NewBuffer(jsonValue))
 	if err != nil {
-		return errors.New(fmt.Sprintf("The HTTP request failed with error %s\n", err))
+		return errors.New(fmt.Sprintf("The HTTP request failed with error %s", err))
 	}
 
 	data, _ := ioutil.ReadAll(response.Body)
 	json.Unmarshal([]byte(data), &jsonResponse)
 
-	fmt.Printf("%v\n", jsonResponse)
+	if jsonResponse.Code != 200 {
+		return errors.New(fmt.Sprintf("Could not send tx. Error code: %s", jsonResponse.Code))
+	}
 
 	return nil
 }
